6b38163bc09e73114a6d4b9a39609d3296dbe82b
Talltiger50 <80235911+Talltiger50@users.noreply.github.com>
2025-06-25 19:46:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,13 @@ python -m venv .venv
 source .venv/bin/activate
 
 pip install -r requirements.txt
-echo "done"
+echo "done installing python packages"
+echo "downloading piper tts model"
 
+mkdir piper-tts
+cd piper-tts
+wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/joe/medium/en_US-joe-medium.onnx?download=true -O en_us_joe_medium.onnx
+wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/joe/medium/en_US-joe-medium.onnx.json?download=true -O en_us_joe_medium.onnx
+echo "done downloading model"
+
+cd ..
