8eb6d82ecdfafcb2c7919eab4380af68fd94e684
Talltiger50 <80235911+Talltiger50@users.noreply.github.com>
2025-06-25 19:39:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-#!bash
-
-
+#!/bin/bash
+echo "Installing..."
 sudo apt install portaudio19-dev
 
 python -m venv .venv
 source .venv/bin/activate
 
 pip install -r requirements.txt
+echo "done"
 
